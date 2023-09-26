355266d79baba5804676d58ddc8488ee2cf8e5a1
Artem Lysenko <artemlysenko@niuitmo.ru>
2023-09-26 13:14:02 +0300

    last commit

--- src/main/java/homeworks/homework1/task3/Multithreading.java
+++ src/main/java/homeworks/homework1/task3/Multithreading.java
@@ -4,31 +4,34 @@ import lombok.Getter;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Multithreading {
     public static void main(String[] args) {
-        Variable variable = new Variable();
-        Runnable runnable = variable::inc;
-
+        AtomicInteger x = new AtomicInteger(1);
         ExecutorService service = Executors.newFixedThreadPool(3);
 
         for (int i = 0; i < 3; i++) {
-            service.execute(runnable);
+            service.execute(() -> {
+                for (int j = 0; j < 10_000; j++) {
+                    x.incrementAndGet();
+                    System.out.println("Thread " + Thread.currentThread().getName() + " count " + x.get());
+                }
+            });
         }
 
-        service.shutdown();
-        while (!service.isTerminated()) {}
-
-        System.out.println(variable.getX());
-    }
-}
-
-@Getter
-class Variable {
-    private int x = 1;
-    public synchronized void inc() {
-        for (int i = 0; i < 10_000; i++) {
-            x += 1;
+        // disable new tasks from being submitted
+        try {
+            service.shutdown();
+            if (!service.awaitTermination(60, TimeUnit.SECONDS)) {
+                System.out.println("Can't stop executing");
+            }
+            else {
+                System.out.println(x.get());
+            }
+        }catch (InterruptedException e) {
+            System.err.println("Some problems");
         }
     }
 }
